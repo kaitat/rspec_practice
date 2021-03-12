949e259214a3dd70ded84ccb5c7cbb32cfa8dc6a
kaitat1231@gmail.com <kaitat1231@gmail.com>
2021-03-12 22:31:56 +0900

    user email ユニークに

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "test@example.test" }
+    sequence(:email) { |n| "TESTRSPEC#{n}@example.com" }
     nick_name { "test user" }
     password { "password" }
   end
