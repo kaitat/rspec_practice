7a2cf6228d5f40402d59ede4c389534a5080d9dc
kaitat1231@gmail.com <kaitat1231@gmail.com>
2021-03-12 22:31:33 +0900

    rspec ログイン実装

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,13 @@
+# devise ログインの処理 先にadminも置いておく
+module ControllerMacros
+  def login_admin(admin)
+    @request.env["devise.mapping"] = Devise.mappings[:admin]
+    sign_in admin
+  end
+
+  def login_user(user)
+    controller.stub(:authenticate_user!).and_return true
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+end
