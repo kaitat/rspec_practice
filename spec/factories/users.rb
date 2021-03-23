f25ba3c760dc9b0f80d5eba257228dacf4968e04
kaitat1231@gmail.com <kaitat1231@gmail.com>
2021-03-23 23:35:12 +0900

    facker使ってみた

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "TESTRSPEC#{n}@example.com" }
-    nick_name { "test user" }
-    password { "password" }
+    # https://github.com/faker-ruby/faker/blob/master/doc/default/internet.md
+    password = Faker::Internet.password(8)
+    email { Faker::Internet.free_email }
+    nick_name { Faker::Internet.username }
+    password { password }
+    password_confirmation { password }
   end
 end
